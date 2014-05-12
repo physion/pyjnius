6f614a6eb31ddd6d534662cec21b30d59f5cab40
Barry Wark <barrywark@gmail.com>
2014-05-11 23:07:33 -0400

    Adds test for utterable

---
+++ tests/org/jnius/SimpleIterable.java
@@ -0,0 +1,11 @@
+package org.jnius;
+
+import java.util.*;
+
+public class SimpleIterable {
+	public Iterable<String> getIterable() {
+        List<String> result = new ArrayList<String>();
+        result.add("hello");
+        result.add("world");
+	}
+}
--- tests/test_reflect.py
+++ tests/test_reflect.py
@@ -11,3 +11,17 @@ class ReflectTest(unittest.TestCase):
         stack.push('world')
         self.assertEqual(stack.pop(), 'world')
         self.assertEqual(stack.pop(), 'hello')
+
+
+class IterableConversion(unittest.TestCase):
+    
+    def test_should_wrap_java_iterable_as_python_iterable(self):
+        
+        SimpleIterable = autoclass('org.jnius.SimpleIterable')
+        
+        si = SimpleIterable()
+        
+        expected = list("hello", "world")
+        actual = list(si.getIterable())
+        
+        self.assertEquals(expected, actual)
